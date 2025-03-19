0c5a64d757ac98db56dcf9701941206360ff7281
251654 Urbaniak Mateusz <251654@edu.p.lodz.pl>
2025-03-19 11:42:31 +0100

    Begining project - Main push

--- Model/src/main/java/DES.java
+++ Model/src/main/java/DES.java
@@ -1,2 +1,9 @@
 public class DES {
+    public static String encrypt(String plainText, String key) {
+        return "encrypted";
+    }
+
+    public static String decrypt(String cipherText, String key) {
+        return "decrypted";
+    }
 }
